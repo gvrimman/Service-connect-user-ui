15d05b32e09f8b0c51fe891bfea4359fa36c51d3
Nithin Raju <nithinraju242@gmail.com>
2024-11-27 17:18:31 +0530

    production launch

--- src/routes/UserRoutes.jsx
+++ src/routes/UserRoutes.jsx
@@ -1,249 +1,312 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
-import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
+import React, { lazy, Suspense } from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
 import { protectRoute } from "../utils/authentication/Auth";
+
 // Authentication Pages
-import UserSignInPage from '../pages/signUpSignInPages/UserSignInPage';
-import UserSignUpPage from '../pages/signUpSignInPages/UserSignUpPage';
-import OTPPage from '../pages/signUpSignInPages/OtpVerificationPage';
-import ForgotPasswordPage from '../pages/signUpSignInPages/forgotPasswordPages/ForgotPasswordPage';
-import CreateNewPasswordPage from '../pages/signUpSignInPages/forgotPasswordPages/CreateNewPasswordPage';
-import OtpForgotPasswordPage from '../pages/signUpSignInPages/forgotPasswordPages/OtpForgotPasswordPage';
-import AddProfilePage from '../pages/signUpSignInPages/AddProfilePage';
+const UserSignInPage = lazy(() =>
+  import("../pages/signUpSignInPages/UserSignInPage")
+);
+const UserSignUpPage = lazy(() =>
+  import("../pages/signUpSignInPages/UserSignUpPage")
+);
+const OTPPage = lazy(() =>
+  import("../pages/signUpSignInPages/OtpVerificationPage")
+);
+const ForgotPasswordPage = lazy(() =>
+  import("../pages/signUpSignInPages/forgotPasswordPages/ForgotPasswordPage")
+);
+const CreateNewPasswordPage = lazy(() =>
+  import("../pages/signUpSignInPages/forgotPasswordPages/CreateNewPasswordPage")
+);
+const OtpForgotPasswordPage = lazy(() =>
+  import("../pages/signUpSignInPages/forgotPasswordPages/OtpForgotPasswordPage")
+);
+const AddProfilePage = lazy(() =>
+  import("../pages/signUpSignInPages/AddProfilePage")
+);
 
 // Home, Profile, and Settings Pages
-import Home from '../pages/userHomePages/HomePage/Home';
-import UserProfile from "../pages/userProfilePage/UserProfile";
-import EditProfile from "../pages/editProfilePage/EditProfile";
-import SecurityPage from "../pages/securityPage/SecurityPage";
-import NotificationSettingsPage from "../pages/NotificationSettingsPage/NotificationSettingsPage";
+const Home = lazy(() => import("../pages/userHomePages/HomePage/Home"));
+const UserProfile = lazy(() => import("../pages/userProfilePage/UserProfile"));
+const EditProfile = lazy(() => import("../pages/editProfilePage/EditProfile"));
+const SecurityPage = lazy(() => import("../pages/securityPage/SecurityPage"));
+const NotificationSettingsPage = lazy(() =>
+  import("../pages/NotificationSettingsPage/NotificationSettingsPage")
+);
 
 // Service Pages
-import ServicesPage from "../pages/serviceProviderListingPage/ServicesPage";
-import RequestServicePage from "../pages/requestServicePages/RequestServicePage";
-import ActiveServicesPage from "../pages/userHomePages/ActiveServicesPage";
-import ActiveServiceDetails from "../pages/BookingDetailsPages/ActiveServiceDetails";
-import CompletedServicesPage from "../pages/userHomePages/CompletedServicesPage";
+const ServicesPage = lazy(() =>
+  import("../pages/serviceProviderListingPage/ServicesPage")
+);
+const RequestServicePage = lazy(() =>
+  import("../pages/requestServicePages/RequestServicePage")
+);
+const ActiveServicesPage = lazy(() =>
+  import("../pages/userHomePages/ActiveServicesPage")
+);
+const ActiveServiceDetails = lazy(() =>
+  import("../pages/BookingDetailsPages/ActiveServiceDetails")
+);
+const CompletedServicesPage = lazy(() =>
+  import("../pages/userHomePages/CompletedServicesPage")
+);
 
 // Category and Provider Pages
-import AllCategoryComponent from "../components/user/allCategoryAndSubCategoryComponent/allCategoryComponent/AllCategoryComponent";
-import SubCategoriesPage from "../pages/allCategoryAndSubCategoryPage/SubCategoryPage";
-import ProviderProfile from '../pages/ProviderProfilePage/ProviderProfile';
+const AllCategoryComponent = lazy(() =>
+  import(
+    "../components/user/allCategoryAndSubCategoryComponent/allCategoryComponent/AllCategoryComponent"
+  )
+);
+const SubCategoriesPage = lazy(() =>
+  import("../pages/allCategoryAndSubCategoryPage/SubCategoryPage")
+);
+const ProviderProfile = lazy(() =>
+  import("../pages/ProviderProfilePage/ProviderProfile")
+);
 
 // Booking and Review Pages
-import BookingsPage from "../pages/userHomePages/BookingsPage";
-import BookingDetails from "../pages/BookingDetailsPages/BookingDetails";
-import ReviewSection from "../components/user/reviewsPageComponents/ReviewSection";
-import ReviewSubmitPage from '../components/user/reviewSubmitComponents/ReviewSubmitPage';
+const BookingsPage = lazy(() => import("../pages/userHomePages/BookingsPage"));
+const BookingDetails = lazy(() =>
+  import("../pages/BookingDetailsPages/BookingDetails")
+);
+const ReviewSection = lazy(() =>
+  import("../components/user/reviewsPageComponents/ReviewSection")
+);
+const ReviewSubmitPage = lazy(() =>
+  import("../components/user/reviewSubmitComponents/ReviewSubmitPage")
+);
 
 // Complaint and Transaction Pages
-import ComplaintsPage from '../pages/userHomePages/ComplaintsPage';
-import ComplaintFormPage from "../pages/ComplaintFormPage/ComplaintFormPage";
-import TransactionsPage from '../pages/transactionsPage/TransactionsPage';
-import ComplaintsPageUser from '../components/user/complaintsPageComponents/ComplaintsPageUser';
+const ComplaintsPage = lazy(() =>
+  import("../pages/userHomePages/ComplaintsPage")
+);
+const ComplaintFormPage = lazy(() =>
+  import("../pages/ComplaintFormPage/ComplaintFormPage")
+);
+const TransactionsPage = lazy(() =>
+  import("../pages/transactionsPage/TransactionsPage")
+);
+const ComplaintsPageUser = lazy(() =>
+  import("../components/user/complaintsPageComponents/ComplaintsPageUser")
+);
 
 // Additional Pages
-import TermsAndConditionPage from "../pages/termsAndConditionPage/TermsAndConditionPage";
-import PaymentMethodPage from "../pages/paymentMethodsPages/PaymentMethodPage";
-import NotificationsPage from "../pages/userHomePages/NotificationPage";
-import HelpCenterPage from "../pages/helpCenterPage/HelpCenterPage";
-import AboutPage from "../pages/aboutPage/AboutPage";
+const TermsAndConditionPage = lazy(() =>
+  import("../pages/termsAndConditionPage/TermsAndConditionPage")
+);
+const PaymentMethodPage = lazy(() =>
+  import("../pages/paymentMethodsPages/PaymentMethodPage")
+);
+const NotificationsPage = lazy(() =>
+  import("../pages/userHomePages/NotificationPage")
+);
+const HelpCenterPage = lazy(() =>
+  import("../pages/helpCenterPage/HelpCenterPage")
+);
+const AboutPage = lazy(() => import("../pages/aboutPage/AboutPage"));
 
 // Layouts and Error Pages
-import MainLayout from "../layouts/UserLayout";
-import Error404Page from "../pages/ErrorPages/Error404Page";
-import Error403Page from "../pages/ErrorPages/Error403Page";
-import AuthLayout from "../layouts/AuthLayout";
-
-
+const MainLayout = lazy(() => import("../layouts/UserLayout"));
+const Error404Page = lazy(() => import("../pages/ErrorPages/Error404Page"));
+const Error403Page = lazy(() => import("../pages/ErrorPages/Error403Page"));
+const AuthLayout = lazy(() => import("../layouts/AuthLayout"));
 
 // Router Configuration
 const UserRouter = createBrowserRouter([
-  { path: "/",
-    element: < AuthLayout/>,
+  {
+    path: "/",
+    element: <AuthLayout />,
     children: [
       // Public Routes
       {
         path: "/",
-        element: <Navigate to="/sign-in" />
+        element: <Navigate to="/sign-in" />,
       },
 
       {
         path: "/sign-in",
-        element: <UserSignInPage />
+        element: <UserSignInPage />,
       },
 
       {
         path: "/sign-up",
-        element: <UserSignUpPage />
+        element: <UserSignUpPage />,
       },
       {
         path: "/otp",
-        element: <OTPPage />
+        element: <OTPPage />,
       },
       {
         path: "/forgot-password",
-        element: <ForgotPasswordPage />
+        element: <ForgotPasswordPage />,
       },
       {
         path: "/create-new-password",
-        element: <CreateNewPasswordPage />
+        element: <CreateNewPasswordPage />,
       },
       {
         path: "/otp-forgot-password",
-        element: <OtpForgotPasswordPage />
+        element: <OtpForgotPasswordPage />,
       },
       {
         path: "/add-profile",
-        element: <AddProfilePage />
+        element: <AddProfilePage />,
       },
       {
         path: "/fill-your-profile",
-        element: <AddProfilePage />
+        element: <AddProfilePage />,
       },
-    ]},{path: "/",
-    element: < MainLayout/>,
+    ],
+  },
+  {
+    path: "/",
+    element: <MainLayout />,
     children: [
       // Protected Routes
       {
         path: "/home",
         element: <Home />,
-        loader: protectRoute
+        loader: protectRoute,
       },
       {
         path: "/profile",
         element: <UserProfile />,
-        loader: protectRoute
+        loader: protectRoute,
       },
       {
         path: "/edit-profile",
         element: <EditProfile />,
-        loader: protectRoute
+        loader: protectRoute,
       },
       {
         path: "/security",
         element: <SecurityPage />,
-        loader: protectRoute
+        loader: protectRoute,
       },
       {
         path: "/notification-settings",
         element: <NotificationSettingsPage />,
-        loader: protectRoute
+        loader: protectRoute,
       },
 
       // Service Routes
       {
         path: "/services",
         element: <CompletedServicesPage />,
-        loader: protectRoute
+        loader: protectRoute,
       },
       {
         path: "/active-services",
         element: <ActiveServicesPage />,
-        loader: protectRoute
+        loader: protectRoute,
       },
       {
         path: "/service-details/:id",
         element: <ActiveServiceDetails />,
-        loader: protectRoute
+        loader: protectRoute,
       },
       {
         path: "/service-provider-list/:id",
         element: <ServicesPage />,
-        loader: protectRoute
+        loader: protectRoute,
       },
       {
         path: "/request-service/:id",
         element: <RequestServicePage />,
-        loader: protectRoute
+        loader: protectRoute,
       },
 
       // Category and Provider Routes
       {
         path: "/categories",
         element: <AllCategoryComponent />,
-        loader: protectRoute
+        loader: protectRoute,
       },
       {
         path: "/subcategories/:categoryId",
         element: <SubCategoriesPage />,
-        loader: protectRoute
+        loader: protectRoute,
       },
       {
         path: "/provider-profile/:id",
         element: <ProviderProfile />,
-        loader: protectRoute
+        loader: protectRoute,
       },
 
       // Booking and Review Routes
       {
         path: "/bookings",
         element: <BookingsPage />,
-        loader: protectRoute
+        loader: protectRoute,
       },
       {
         path: "/booking-details/:id",
         element: <BookingDetails />,
-        loader: protectRoute
+        loader: protectRoute,
       },
       {
         path: "/reviews",
         element: <ReviewSection />,
-        loader: protectRoute
+        loader: protectRoute,
       },
       {
         path: "/review-submit/:id",
         element: <ReviewSubmitPage />,
-        loader: protectRoute
+        loader: protectRoute,
       },
 
       // Complaint and Transaction Routes
       {
         path: "/complaints",
         element: <ComplaintsPage />,
-        loader: protectRoute
+        loader: protectRoute,
       },
       {
         path: "/complaint-submit/:id",
         element: <ComplaintFormPage />,
-        loader: protectRoute
+        loader: protectRoute,
       },
       {
         path: "/complaint-form/:id",
         element: <ComplaintsPageUser />,
-        loader: protectRoute
+        loader: protectRoute,
       },
       {
         path: "/transactions",
         element: <TransactionsPage />,
-        loader: protectRoute
+        loader: protectRoute,
       },
       {
         path: "/payment-methods/:id",
         element: <PaymentMethodPage />,
-        loader: protectRoute
+        loader: protectRoute,
       },
 
       // Additional Routes
       {
         path: "/notification",
         element: <NotificationsPage />,
-        loader: protectRoute
+        loader: protectRoute,
       },
       {
         path: "/terms-conditions",
         element: <TermsAndConditionPage />,
-        loader: protectRoute
+        loader: protectRoute,
       },
       {
         path: "/help-center",
         element: <HelpCenterPage />,
-        loader: protectRoute
+        loader: protectRoute,
       },
       {
         path: "/about-us",
         element: <AboutPage />,
-        loader: protectRoute
+        loader: protectRoute,
       },
     ],
   },
@@ -251,5 +314,8 @@ const UserRouter = createBrowserRouter([
   { path: "/403", element: <Error403Page /> },
 ]);
 
+<Suspense fallback={<div>Loading...</div>}>
+  <RouterProvider router={UserRouter} />
+</Suspense>;
 
-export default UserRouter ;
+export default UserRouter;
